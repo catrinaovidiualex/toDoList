8c24e1458326dcd1adba59ea589edc38747048d0
Catrina Alex <ovidiualex.catrina@gmail.com>
2021-11-21 12:46:44 +0200

    other commit

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
-let btnAdd=document.querySelector(".adaugare");
+let tasks=[
 
-btnAdd.addEventListener("click",()=>{
-    
-})
+{taskName:"Creating new project with JS, CSS and HTML",
+status:"Finished"
+
+},
+{taskName:"Learning about DOM Manipulation",
+status:"In Progress"
+
+},
+{taskName:"Updating GitHub projects",
+status:"To Do"
+
+}
+
+];
+displayTable();
+
+function displayTable(){
+    let table=document.querySelector("table");
+
+    tasks.forEach(task=>{
+        let tableRow=document.createElement("tr");
+        let name=document.createElement("td");
+        let status=document.createElement("td");
+        let deleteTask=document.createElement("td");
+
+        name.innerText=tasks.taskName;
+
+        tableRow.appendChild(name);
+        tableRow.appendChild(name);
+
+
+
+
+
+
+    })
+}
 
